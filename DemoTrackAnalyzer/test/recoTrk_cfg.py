04e9e3cbb2e3c90ac674cc9c70095838e3d1ee7d
Marco Rovere <marco.rovere@cern.ch>
2014-09-03 15:32:36 +0200

    Adapt code to run on 72X

--- DemoTrackAnalyzer/test/recoTrk_cfg.py
+++ DemoTrackAnalyzer/test/recoTrk_cfg.py
@@ -8,21 +8,24 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 ### source
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    '/store/relval/CMSSW_7_0_0_pre5/RelValSingleMuPt10/GEN-SIM-RECO/PRE_ST62_V8-v1/00000/26B1495D-F42F-E311-A3B5-002618943800.root'
+    '/store/relval/CMSSW_7_2_0_pre3/RelValTTbar_13/GEN-SIM-RECO/PU25ns_POSTLS172_V3_CondDBv2-v1/00000/7E6493F0-5F21-E411-90CD-0025905A613C.root',
+    '/store/relval/CMSSW_7_2_0_pre3/RelValTTbar_13/GEN-SIM-RECO/PU25ns_POSTLS172_V3_CondDBv2-v1/00000/A22ECB6B-5F21-E411-9968-0025905B8576.root',
+    '/store/relval/CMSSW_7_2_0_pre3/RelValTTbar_13/GEN-SIM-RECO/PU25ns_POSTLS172_V3_CondDBv2-v1/00000/C0FABB8A-B021-E411-80D0-0025905B85A2.root',
+    '/store/relval/CMSSW_7_2_0_pre3/RelValTTbar_13/GEN-SIM-RECO/PU25ns_POSTLS172_V3_CondDBv2-v1/00000/EE538882-B021-E411-B970-0025905B85AE.root'
     )
 )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 ### conditions
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-# same as input file
-process.GlobalTag.globaltag = 'PRE_ST62_V8::All'
+# Load Manually
+#process.GlobalTag.globaltag = 'POSTLS172_V4::All'
 # or get it automatically
-#from Configuration.AlCa.GlobalTag import GlobalTag
-#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:startup', '')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
 
 ### standard includes (can't live without)
-process.load('Configuration/StandardSequences/Services_cff')
+process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.Geometry_cff')
 process.load("Configuration.StandardSequences.RawToDigi_cff")
 process.load("Configuration.EventContent.EventContent_cff")
@@ -34,15 +37,16 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 process.clustToHits = cms.Sequence(
     process.siPixelRecHits*process.siStripMatchedRecHits
 )
+
+# This is needed for tracking to work properly
+process.load("RecoTracker.MeasurementDet.MeasurementTrackerEventProducer_cfi")
+process.load("RecoPixelVertexing.PixelLowPtUtilities.siPixelClusterShapeCache_cfi")
 # this is the official tracking sequence
 process.tracking = cms.Sequence(
-    process.trackingGlobalReco
+    process.MeasurementTrackerEvent
+    + process.siPixelClusterShapeCache
+    + process.trackingGlobalReco
 )
-
-process.pixellayertriplets.layerList = cms.vstring('BPix1+BPix2+FPix1_pos',
-                                                   'BPix1+BPix2+FPix1_neg',
-                                                   'BPix1+FPix1_pos+FPix2_pos',
-                                                   'BPix1+FPix1_neg+FPix2_neg')
 
 ### define output EDMFile
 MyReRecoEventContent = cms.PSet(
@@ -66,7 +70,8 @@ process.p = cms.Path(process.clustToHits * process.tracking)
 process.o = cms.EndPath(process.out)
 
 ### sequence of paths to run
-process.schedule = cms.Schedule(process.p, process.o)
+process.schedule = cms.Schedule(process.p,
+                                process.o)
 
 ### debug time and memory information (more at Validation/Performance/python//TimeMemory*.py)
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
